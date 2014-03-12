f90c940c160bd39ed0b58aeb29efe07911d19f97
Mike McLean <mikem@redhat.com>
2014-03-12 14:15:05 -0400

    ...

--- explode_srpm.sh
+++ explode_srpm.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 rpm -iv --define "_topdir $PWD" "$@"
+
+#TODO - handle .X.metadata and .gitignore
+#see https://nazar.karan.org/blob/nazar-repos/HEAD/sync_sources.sh
+
+#TODO - integrate with pyrpkg
+
+
