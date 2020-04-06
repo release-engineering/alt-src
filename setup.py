90a60726fe91a7289872e609a30a843959b44985
Rajul Kumar <rajkumar@redhat.com>
2020-04-06 19:24:30 -0400

    Remove Topic from classifiers in setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
             "Programming Language :: Python :: 2.6",
             "Programming Language :: Python :: 2.7",
             "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-            "Topic :: Software Development :: Libraries :: Python Modules",
       ],
       install_requires=get_requirements(),
       entry_points={
